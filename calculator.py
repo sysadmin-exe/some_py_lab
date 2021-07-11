3fa2331287526d10074f887219c1104ffa9e151c
Victor Efedi <victorefedi@Victors-MacBook-Pro-2.local>
2021-07-12 00:16:15 +0100

    more code

--- calculator.py
+++ calculator.py
@@ -1,14 +1,20 @@
-num1 = int(input('Enter first number: '))
-num2 = int(input('Enter second number: '))
-op = input('Enter operator: ')
 
-if op == '+':
-  print('The addition is = ', num1+num2)
-elif op == '-':
-  print('The substraction is = ', num1-num2)
-elif op == '*':
-  print('The multiplication is = ', num1*num2)
-elif op == '/':
-  print('The division is = ', num1/num2)
-else:
-  print('Invalid Operator')
+def calc(num1, num2, op):
+  if op == '+':
+    print('The addition is = ', num1+num2)
+  elif op == '-':
+    print('The substraction is = ', num1-num2)
+  elif op == '*':
+    print('The multiplication is = ', num1*num2)
+  elif op == '/':
+    print('The division is = ', num1/num2)
+  else:
+    print('Invalid Operator')
+
+def main():
+  num1 = int(input('Enter first number: '))
+  num2 = int(input('Enter second number: '))
+  op = input('Enter operator: ')
+  calc(num1, num2, op)
+
+main()
