0aaf2e69069ff6a7434bb1bd35ebbda3cdd71439
Victor Efedi <victorefedi@Victors-MacBook-Pro-2.local>
2021-07-22 19:38:59 +0100

    form with word counter backend api using POST

--- project1/api1/urls.py
+++ project1/api1/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('', views.index, name='index')
+  #path('', views.index, name='index')
+  path('', views.form, name='form'),
+  path('counter', views.counter, name='counter'),
+  path('api1', views.api1, name='api1')
 ]
--- project1/api1/views.py
+++ project1/api1/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
+# Create your application function or class here.
 def index(request):
   name = 'Victor'
   context = {
@@ -9,3 +9,14 @@ def index(request):
     'age': 23
   }
   return render(request, 'index.html', context)
+
+def form(request):
+  return render(request, 'form.html')
+
+def counter(request):
+  text = request.POST['text']
+  number_of_words = len(text.split())
+  return render(request, 'counter.html', {'number': number_of_words})
+
+def api1(request):
+  return render(request, 'api1.html')
