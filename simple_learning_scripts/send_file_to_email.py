b32e7bc3ad26e4aaec252b3c6835e38d5646df49
sysadmin-exe <50541611+sysadmin-exe@users.noreply.github.com>
2021-09-16 19:00:23 +0100

    added script to send email with python

---
+++ simple_learning_scripts/send_file_to_email.py
@@ -0,0 +1,41 @@
+import os
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
+
+def send_to_email():
+  mail_list = [] #list of email recipients
+
+  for mail in mail_list:
+    email_username = os.environ.get('EMAIL_USERNAME') # email sender email
+    email_passwd = os.environ.get('EMAIL_PASSWD') #email sender passwd
+
+    username = f"{email_username}"
+    password = f"{email_passwd}"
+    mail_from = f"{email_username}"
+    mail_subject = f"{email subject}" # subject of email
+    mail_body = "HELLO, THIS IS A SAMPLE EMAIL" # email body in text format
+    mail_attachment=f"{src_file}" # local file to be added as attachment
+    mail_attachment_name=f"{src_file}" # name of attachment as shown in email
+
+    mimemsg = MIMEMultipart()
+    mimemsg['From']=mail_from
+    mimemsg['To']=mail
+    mimemsg['Subject']=mail_subject
+    mimemsg.attach(MIMEText(mail_body, 'plain'))
+
+    with open(mail_attachment, "rb") as attachment:
+        mimefile = MIMEBase('application', 'octet-stream')
+        mimefile.set_payload((attachment).read())
+        encoders.encode_base64(mimefile)
+        mimefile.add_header('Content-Disposition', "attachment; filename= %s" % mail_attachment_name)
+        mimemsg.attach(mimefile)
+        connection = smtplib.SMTP(host='smtp.office365.com', port=587)
+        connection.starttls()
+        connection.login(username,password)
+        connection.send_message(mimemsg)
+        connection.quit()
+
+send_to_email()
